49a958e5408947f09ab2b853de3d304dc50d7d38
anamerasul <anamerasul@gmail.com>
2022-04-20 20:35:43 +0600

    Create dynamic api, api parameter, access params

---
+++ dynamic-api/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("HELLO FROM dynamc")
+});
+
+
+const users = [
+    { id: 1, name: "jasim", email: "jasim1@j.com" },
+    { id: 2, name: "jasim3", email: "jasim2@j.com" },
+    { id: 3, name: "jasim4", email: "jasim6@j.com" },
+    { id: 4, name: "jasim5", email: "jasim5@j.com" }
+]
+
+app.get("/users", (req, res) => {
+    res.send(users)
+
+});
+
+app.get("/user/:id", (req, res) => {
+
+    console.log(req.params)
+    const id = +(req.params.id)
+    const user = users[id];
+    res.send(user)
+
+});
+app.listen(port, () => {
+    console.log("Lisening to port", port)
+})
